933978b7911071ee247c559a34f5ea74a964dfe1
JadenYee <xxxJadenYee@gmail.com>
2020-07-15 12:21:32 +0800

    不使用内置的tomcat

--- src/main/java/tk/com/MainApplication.java
+++ src/main/java/tk/com/MainApplication.java
@@ -2,10 +2,18 @@ package tk.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class);
+
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
     }
 }
